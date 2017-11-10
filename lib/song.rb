c1a7ae6ea057df1ff37cd96845cf65367aedcfde
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-10 03:00:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,6 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
@@ -18,16 +14,8 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
   def to_param
     name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
   end
 
   def self.all
